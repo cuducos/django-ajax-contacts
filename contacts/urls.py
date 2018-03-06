94bf620ed1228abedf88cf2d66c59bfb9f990ec7
Eduardo Cuducos <cuducos@gmail.com>
2018-03-05 22:07:43 -0300

    Add HTML template

--- contacts/urls.py
+++ contacts/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
+from contacts.core.views import home
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home')
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
